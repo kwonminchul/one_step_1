8ea5c3e2353a971fc8a31fc2e34b432a2a28cdf9
kwonminchul <kmc3853@gmail.com>
2023-01-20 00:30:43 +0900

    createUser test

--- src/components/commons/createuser/01/createUser01.queries.tsx
+++ src/components/commons/createuser/01/createUser01.queries.tsx
@@ -3,7 +3,7 @@ import { gql } from "@apollo/client";
 export const CREATE_USER = gql`
   mutation createUser($createUserInput: CreateUserInput!) {
     createUser(createUserInput: $createUserInput) {
-      _id
+      # _id
       name
       email
       passsword
